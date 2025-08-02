9d735b34f2eb81cce845cfa7c3c108f82181aeec
dawid288 <daw.pisar@interia.pl>
2025-08-02 11:17:03 +0200

    fccu: create fan structure

--- app/src/fccu_v2.h
+++ app/src/fccu_v2.h
@@ -4,23 +4,15 @@
 
 #include "can.h"
 #include "gpio.h"
+#include "pwm.h"
 #include "adc.h"
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/sensor_data_types.h>
+#include <zephyr/rtio/rtio.h>
+#include <zephyr/dsp/print_format.h>
 
 
-// typedef struct {
-//     struct adc_dt_spec low_pressure_sensor;
-//     struct adc_dt_spec fuel_cell_voltage;
-//     struct adc_dt_spec supercap_voltage;
-//     struct adc_dt_spec temp_sensor;
-// }fccu_adc_channel_t;
-//
-// typedef struct {
-//     int16_t low_pressure_sensor_value;
-//     int16_t fuel_cell_voltage_value;
-//     int16_t supercap_voltage_value;
-//     int16_t temp_sensor_value;
-// }fccu_adc_measurement_t;
 
 typedef struct {
     struct adc_dt_spec adc_channel;
@@ -35,25 +27,51 @@ typedef struct {
     fccu_adc_device_t temp_sensor;
 }fccu_adc_t;
 
+typedef struct {
+    struct pwm_dt_spec fan_pwm;
+    struct gpio_dt_spec fan_on_pin;
+}fccu_fan_t;
+
+
+
+
+
+
 
 typedef struct {
     struct gpio_dt_spec can_status_led;
-    struct gpio_dt_spec fan_on_pin;
+
     struct gpio_dt_spec main_valve_on_pin;
     struct gpio_dt_spec purge_valve_on_pin;
 }fccu_gpio_t;
 
-typedef const struct device *fccu_can_device_t;
+typedef const struct device *fccu_can_t;
+typedef const struct device *bmp280_sensor_t;
+
 
 typedef struct {
     fccu_gpio_t fccu_gpio;
     fccu_adc_t fccu_adc;
-    fccu_can_device_t fccu_can_device;
+    fccu_can_t fccu_can;
+    bmp280_sensor_t bmp280_sensor;
+
 }fccu_device_t;
 
 void fccu_init(fccu_device_t *fccu_device);
 void fccu_gpio_init(fccu_gpio_t *fccu_gpio);
 void fccu_adc_init(fccu_adc_t *fccu_adc);
+void fccu_can_init(fccu_can_t *can_device);
+void_fccu_fan_init()
+
+
+
+
+
+void fccu_bmp280_sensor_init(bmp280_sensor_t *sensor);
+
+void fccu_bmp280_sensor_read(bmp280_sensor_t *sensor);
+
+
 void fccu_adc_read(fccu_adc_t *fccu_adc);
 void fccu_on_tick(fccu_device_t* fccu_device);
 
