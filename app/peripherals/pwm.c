444ca402f848f365957856c5d7a231abee1bf840
dawid288 <daw.pisar@interia.pl>
2025-08-02 10:39:28 +0200

    pwm: add function to set pwm pulse width in percent

--- app/peripherals/pwm.c
+++ app/peripherals/pwm.c
@@ -5,3 +5,15 @@ void pwm_init(struct pwm_dt_spec *pwm) {
         printf("Error: PWM device %s is not ready\n", pwm->dev->name);
     }
 }
+
+int pwm_set_pulse_width_percent(struct pwm_dt_spec *pwm, uint8_t pulse_width_percent) {
+    if (pulse_width_percent > 100) pulse_width_percent = 100;
+
+    uint32_t pulse_width = (pwm->period * pulse_width_percent) / 100;
+
+    int ret = pwm_set_pulse_dt(pwm, pulse_width);
+    if (ret < 0) {
+        printf("Error %d: failed to set pulse width\n", ret);
+    }
+    return ret;
+}
--- app/peripherals/pwm.h
+++ app/peripherals/pwm.h
@@ -4,6 +4,7 @@
 #include <zephyr/drivers/pwm.h>
 
 void pwm_init(struct pwm_dt_spec pwm);
+int pwm_set_pulse_width_percent(struct pwm_dt_spec *pwm, uint8_t pulse_width_percent);
 
 
 #endif //PWM_H
