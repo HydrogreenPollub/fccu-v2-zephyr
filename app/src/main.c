1b12f93b50132c3204a05772588f319dbff9767e
dawid288 <daw.pisar@interia.pl>
2025-08-02 16:58:29 +0200

    main: clean main from quick tests

--- app/src/main.c
+++ app/src/main.c
@@ -12,18 +12,17 @@
 
 LOG_MODULE_REGISTER(app);
 
-fccu_device_t fccu;
+// fccu_device_t fccu;
 
 int main() {
-    // // printf("Starting program on board: %s\n", CONFIG_BOARD);
+    printf("Starting program on board: %s\n", CONFIG_BOARD);
 
-    // can_init(can_dev, 500000);
+    // fccu_init(&fccu);
 
-    fccu_init(&fccu);
 
     while (1) {
-        fccu_on_tick(&fccu);
-        // can_send_(can_dev, 24, data, 2);
+        //fccu_on_tick(&fccu);
+
 
 
     }
