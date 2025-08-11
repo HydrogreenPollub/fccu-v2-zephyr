36ce998e64872d8ed5683b34c23eaedec7cb283c
dawid288 <daw.pisar@interia.pl>
2025-08-11 20:48:15 +0200

    fccu: change values in ads1015 struct to float, add to fccu struct ads1015 data and device

--- app/src/fccu_v2.h
+++ app/src/fccu_v2.h
@@ -9,6 +9,7 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/sensor_data_types.h>
 #include <zephyr/logging/log.h>
+#include "ads1015.h"
 
 typedef struct {
     struct adc_dt_spec adc_channel;
@@ -25,10 +26,10 @@ typedef struct {
 }fccu_adc_t;
 
 typedef struct {
-    fccu_adc_device_t low_pressure_sensor;
-    fccu_adc_device_t high_pressure_sensor;
-    fccu_adc_device_t fuel_cell_current;
-}ads1015_adc_t;
+    float low_pressure_sensor;
+    float high_pressure_sensor;
+    float fuel_cell_current;
+}ads1015_adc_data_t;
 
 typedef struct {
     struct pwm_dt_spec fan_pwm;
@@ -71,7 +72,8 @@ typedef struct {
     fccu_adc_t adc;
     fccu_can_t can;
     bmp280_sensor_t bmp280_sensor;
-    ads1015_adc_t ads1015_adc;
+    ads1015_adc_data_t ads1015_data;
+    ads1015_type_t ads1015_device;
 }fccu_device_t;
 
 extern fccu_flags_t flags;
@@ -88,7 +90,8 @@ void fccu_ads1015_adc_init(ads1015_adc_t *adc);
 
 
 void fccu_bmp280_sensor_read(bmp280_sensor_t *sensor);
-void fccu_adc_read(fccu_adc_t *fccu_adc);
+void fccu_adc_read(fccu_adc_t *adc);
+void fccu_ads1015_adc_read(ads1015_adc_t *adc);
 
 void fccu_on_tick(fccu_device_t* fccu);
 
