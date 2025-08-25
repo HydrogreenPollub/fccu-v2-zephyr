21c81c2506b62f54bdd882d0bed1a781f50156e8
dawid288 <daw.pisar@interia.pl>
2025-08-25 22:01:01 +0200

    counter: Add callback setting by top value of counter

--- app/peripherals/counter.c
+++ app/peripherals/counter.c
@@ -15,15 +15,23 @@ void counter_init(const struct device *counter_dev) {
     LOG_INF("Counter initialized");
 }
 
-void counter_set_alarm(const struct device *counter_dev, uint8_t channel_id, counter_alarm_callback_t callback, uint32_t microseconds) {
-    struct counter_alarm_cfg alarm_cfg = {
+void counter_set_alarm(const struct device *counter_dev, uint8_t channel_id, counter_top_callback_t callback, uint32_t microseconds) {
+    // struct counter_alarm_cfg alarm_cfg = {
+    //     .flags = 0,
+    //     .ticks = counter_us_to_ticks(counter_dev, microseconds),
+    //     .callback = callback,
+    //     .user_data = NULL
+    // };
+    // int err = counter_set_channel_alarm(counter_dev, channel_id, &alarm_cfg);
+
+    struct counter_top_cfg top_cfg = {
+        .ticks = counter_us_to_ticks(counter_dev, microseconds), // 1 sekunda
         .flags = 0,
-        .ticks = counter_us_to_ticks(counter_dev, microseconds),
         .callback = callback,
-        .user_data = NULL
+        .user_data = NULL,
     };
 
-    int err = counter_set_channel_alarm(counter_dev, channel_id, &alarm_cfg);
+    int err = counter_set_top_value(counter_dev, &top_cfg);
     if (err < 0)
         LOG_ERR("Failed to set counter alarm (%d)", err);
 
--- app/peripherals/counter.h
+++ app/peripherals/counter.h
@@ -6,7 +6,7 @@
 #include <zephyr/logging/log.h>
 
 void counter_init(const struct device *counter_dev);
-void counter_set_alarm(const struct device *counter_dev, uint8_t channel_id, counter_alarm_callback_t callback, uint32_t microseconds);
-
+// void counter_set_alarm(const struct device *counter_dev, uint8_t channel_id, counter_alarm_callback_t callback, uint32_t microseconds);
+void counter_set_alarm(const struct device *counter_dev, uint8_t channel_id, counter_top_callback_t callback, uint32_t microseconds);
 
 #endif //COUNTER_H
