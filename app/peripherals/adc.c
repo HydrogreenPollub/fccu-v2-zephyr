800be869035d00ea20840777c55f7bc1f870110c
dawid288 <daw.pisar@interia.pl>
2025-08-31 09:28:03 +0200

    adc: define adc sequence within function

--- app/peripherals/adc.c
+++ app/peripherals/adc.c
@@ -2,7 +2,7 @@
 
 LOG_MODULE_REGISTER(adc);
 
-int adc_init(const struct adc_dt_spec *adc, struct adc_sequence *sequence, int16_t *buffer) {
+int adc_init(const struct adc_dt_spec *adc/*, struct adc_sequence *sequence, int16_t *buffer*/) {
     if (!device_is_ready(adc->dev)) {
         LOG_ERR("ADC device not ready\n");
         return -1;
@@ -13,25 +13,25 @@ int adc_init(const struct adc_dt_spec *adc, struct adc_sequence *sequence, int16
         return -1;
     }
 
-    *sequence = (struct adc_sequence){
-        .channels = BIT(adc->channel_id),
-        .buffer = buffer,
-        .buffer_size = sizeof(buffer),
-        .resolution = adc->resolution,
-    };
     return 0;
 }
 
-int adc_read_(const struct adc_dt_spec *adc, struct adc_sequence *sequence) {
-    int ret = adc_read_dt(adc, sequence);
+int adc_read_(const struct adc_dt_spec *adc, int16_t *buffer) {
+
+    struct adc_sequence sequence = {
+        .channels = BIT(adc->channel_id),
+        .buffer = buffer,
+        .buffer_size = sizeof(*buffer),
+        .resolution = adc->resolution,
+    };
+
+    int ret = adc_read_dt(adc, &sequence);
 
     if (ret != 0) {
         LOG_ERR("ADC read failed (err %d)", ret);
-    } else {
-        int16_t *value = sequence->buffer;
-        LOG_INF("ADC raw value: %d \n", value[0]);
+        return ret;
     }
-    return ret;
+    return 0;
 }
 
 float adc_map(float x, float in_min, float in_max, float out_min, float out_max)
